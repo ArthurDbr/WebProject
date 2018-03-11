50b1f8d761fd3684b7963f5fca8a6e80aaa5d0b2
boirosma2 <boiro.yero.sma2@gmail.com>
2018-03-11 19:18:47 +0100

    Fichier controller du formulaire

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -1,9 +1,9 @@
 <?php
 // src/Controller/RegistrationController.php
-namespace App\Controller;
+namespace AppBundle\Controller;
 
-use App\Form\UserType;
-use App\Entity\User;
+use AppBundle\Form\RegistrationFormType;
+use AppBundle\Entity\Users;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,16 +17,16 @@ class RegistrationController extends Controller
     public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
         // 1) build the form
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
+        $user = new Users();
+        $form = $this->createForm(RegistrationFormType::class, $user);
 
         // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
             // 3) Encode the password (you could also do this via Doctrine listener)
-            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-            $user->setPassword($password);
+            //$password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+          //  $user->setPassword($password);
 
             // 4) save the User!
             $entityManager = $this->getDoctrine()->getManager();
@@ -36,11 +36,11 @@ class RegistrationController extends Controller
             // ... do any other work - like sending them an email, etc
             // maybe set a "flash" success message for the user
 
-            return $this->redirectToRoute('replace_with_some_route');
+            return $this->redirectToRoute('/{_locale}/Profil');
         }
 
         return $this->render(
-            'registration/register.html.twig',
+            'Profil/register.html.twig',
             array('form' => $form->createView())
         );
     }
