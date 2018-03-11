9926c131435f21e96100d7368650a4d427f659aa
boirosma2 <boiro.yero.sma2@gmail.com>
2018-03-11 17:26:21 +0100

    resolution d'erreur du formulaire

--- src/AppBundle/Form/RegistrationFormType.php
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -1,7 +1,7 @@
 <?php
 namespace AppBundle\Form;
 
-    use Symfony\Component\Form\FormBuilder;
+    use Symfony\Component\Form\FormBuilderInterface;
     use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
 
     class RegistrationFormType extends BaseType
@@ -9,17 +9,17 @@ namespace AppBundle\Form;
       /**
        * {@inheritdoc}
        */
-        public function buildForm(FormBuilder $builder, array $options)
+        public function buildForm(FormBuilderInterface $builder, array $options)
         {
             parent::buildForm($builder, $options);
 
             // Ajoutez vos champs ici, revoilà notre champ *location* :
-            $builder->add('nom')->('age')->('prenom')->('id');
+            $builder->add('nom')->add('age')->add('prenom');
         }
 
-      /*  public function getName()
+       public function getName()
         {
-            return 'AppBundle_users';
-        }*/
+            return 'AppBundle_users_registration';
+        }
     }
 ?>
