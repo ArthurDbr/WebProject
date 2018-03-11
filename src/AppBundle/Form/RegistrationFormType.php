7a0fc536f9e666e155709f120f676a6e8109a1f6
boirosma2 <boiro.yero.sma2@gmail.com>
2018-03-11 16:22:25 +0100

    formulaire avec suelement nom et id

---
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -0,0 +1,23 @@
+<?php
+namespace AppBundle\Form;
+
+    use Symfony\Component\Form\FormBuilder;
+    use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+    class RegistrationFormType extends BaseType
+    {
+        public function buildForm(FormBuilder $builder, array $options)
+        {
+            parent::buildForm($builder, $options);
+
+            // Ajoutez vos champs ici, revoilà notre champ *location* :
+            $builder->add('nom');
+            $builder->add('id';)
+        }
+
+        public function getName()
+        {
+            return 'myapp_user_registration';
+        }
+    }
+?>
